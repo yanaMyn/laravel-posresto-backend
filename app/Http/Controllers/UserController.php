285d3dfc10391640e02e4b455366621da1687eb8
Yana Mulyana <yanamyn15@gmail.com>
2024-02-10 20:40:05 +0700

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $users = \App\Models\User::paginate(10);
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+        // $users = DB::table('users')->paginate(10);
+        // return view('pages.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        // $data = $request->all();
+        // $data['password'] = Hash::make($request->password);
+        // \App\Models\User::create($data);
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role' => 'required|in:admin,staff,user',
+        ]);
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = $request->role;
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User successfully updated');
+    }
+
+    public function edit($id)
+    {
+        $user = \App\Models\User::findOrFail($id);
+        return view('pages.users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // $data = $request->validated();
+        // $user->update($data);
+        // return redirect()->route('user.index')->with('success', 'User successfully updated');
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'role' => 'required',
+        ]);
+
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User successfully updated');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('user.index')->with('success', 'User successfully deleted');
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">POS RESTO</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('users.index') }}">All Users</a>
+                    </li>
+
+                </ul>
+            </li>
+    </aside>
+</div>
