262a597e16aa7c616158f14943a16bef10d2463b
Yana Mulyana <yanamyn15@gmail.com>
2024-02-21 22:33:53 +0700

    add auth login logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,33 +13,42 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            "email"=> "required|email",
-            "password"=> "required"
+            "email" => "required|email",
+            "password" => "required"
         ]);
 
         //check if the user exists
         $user = User::where("email", $request->email)->first();
         if (!$user) {
             return response()->json([
-                "status"=> "error",
-                "message"=> "user not found"
+                "status" => "error",
+                "message" => "user not found"
             ], 404);
         }
 
         //check if the password is correct
         if (!Hash::check($request->password, $user->password)) {
             return response()->json([
-                "status"=> "error",
-                "message"=> "invalid credentials"
+                "status" => "error",
+                "message" => "invalid credentials"
             ], 401);
         }
 
         //generate token
         $token = $user->createToken("auth-token")->plainTextToken;
         return response()->json([
-            "status"=> "success",
-            "token"=> $token,
-            "user"=> $user
+            "status" => "success",
+            "token" => $token,
+            "user" => $user
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Logged out',
         ], 200);
     }
 }
